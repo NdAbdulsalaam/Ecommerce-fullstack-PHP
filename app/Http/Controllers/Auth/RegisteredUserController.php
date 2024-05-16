e9fc345373b50f62d7e1f462e7aaee550466490a
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-05-16 09:40:58 +0100

    uses the master layout from techquest

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -48,7 +48,8 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
+        $role = Auth::user()->role;
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route($role. '.dashboard', absolute: false));
     }
 }
--- resources/views/seller/dashboard.blade.php
+++ resources/views/seller/dashboard.blade.php
@@ -307,7 +307,7 @@
                 <div class="card-body">
                     <div class="text-center">
                         <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;"
-                            src="img/undraw_posting_photo.svg" alt="...">
+                        src="{{ asset('dashboard/img/undraw_posting_photo.svg') }}"  alt="...">
                     </div>
                     <p>Add some quality, svg illustrations to your project courtesy of <a
                             target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a
